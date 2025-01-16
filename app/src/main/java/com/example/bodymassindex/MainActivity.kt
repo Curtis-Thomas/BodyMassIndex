6412aba8b276e6c6df5bb48eb21807af0b8f41e2
fuzzyduckers <83663235+fuzzyduckers@users.noreply.github.com>
2025-01-16 08:44:57 +0200

    Test

--- app/src/main/java/com/example/bodymassindex/MainActivity.kt
+++ app/src/main/java/com/example/bodymassindex/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             BodyMassIndexTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Test",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
